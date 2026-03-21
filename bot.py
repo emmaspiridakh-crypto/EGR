7b447b76997512f96fefcc7ea51ba36cb4b2e3f0
spemmanouela-hub <spemmanouela@gmail.com>
2026-03-21 23:57:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 # ============================================
 # SECTION 1 — IMPORTS & FLASK KEEP_ALIVE
 # ============================================
-with open("token.txt", "r") as f:
-    TOKEN = f.read().strip()
-
 print(">>> BOT FILE LOADED <<<")
 
 import os
