9913dd62686db6faf9d33f70b3ea1b904faf7adc
spemmanouela-hub <spemmanouela@gmail.com>
2026-03-24 19:55:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -507,7 +507,7 @@ if log_channel:
     log_embed.add_field(name="Τύπος", value=ticket_type)
     log_embed.add_field(name="Channel", value=channel.mention)
 
-await log_channel.send(embed=log_embed)
+    await log_channel.send(embed=log_embed)
 
 await interaction.response.send_message(
     f"Το ticket σου δημιουργήθηκε: {channel.mention}",
@@ -617,7 +617,7 @@ if log_channel:
     log_embed.add_field(name="Τύπος", value=ticket_type)
     log_embed.add_field(name="Channel", value=channel.mention)
 
-await log_channel.send(embed=log_embed)
+    await log_channel.send(embed=log_embed)
 
 await interaction.response.send_message(
     f"Το ticket σου δημιουργήθηκε: {channel.mention}",
